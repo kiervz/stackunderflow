00f56428ac79194aeb72c0036553434b35b98447
kiervz <kierveymaghanoy24@gmail.com>
2020-08-14 15:09:42 +0800

    DONE: like and unlike reply

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,6 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@likeIt')->name('like.like');
+Route::delete('/like/{reply}', 'LikeController@unlikeIt')->name('like.unlike');
